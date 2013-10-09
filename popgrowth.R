1c8cd334148e430b01d17a2b15c24895bceb317e
Katie Montovan <kmontovan@bennington.edu>
2013-10-09 09:27:10 -0400

    Added comments and removed unnecesary spaces.
    This was only a formatting adjustment. No code was changed.

--- popgrowth.R
+++ popgrowth.R
@@ -6,35 +6,21 @@ n3 = n2 * r; # population size at t = 3
 n4 = n3 * r; # population size at t = 4
 n5 = n4 * r; # population size at t = 5
 
-
-
-
-
-
+#Vector form. Creating and adding to a vector.
 popsize = c(n0, n1, n2, n3, n4, n5); popsize; # builds vector popsize with 6 values
-
-
-
-
-
-
-
 popsize[7] = popsize[6]*r; # appending n6 at the end of a row vector containing n0 through n5.
 
-
-
-
-
+#Loops to make calculations more efficient
 r=2; # growth rate
 popsize = 20; #defining initial conditions
 for(time in 2:4){ #defining the start and end of the loop
   popsize[time]=popsize[time-1]*r # the instruction to be repeated
 } #closing the loop
 
-
+#Plotting the results
 plot(popsize)
 
-
+#Plotting the results better
 numiterations = length(popsize); # number of values in popsize
 numiterations = numiterations-1; # because we start with an initial
 # condition at popsize(1), we actually
